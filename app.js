afcd8da1630fd0febc15ae4447a3d1700b28ee1a
Snehal2020 <106869309+Snehal2020@users.noreply.github.com>
2024-04-08 15:49:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,50 +1,50 @@
 const express=require('express');
-// const fs = require('fs');
-// require('./db')
-// const model = require('./Model')
+const fs = require('fs');
+require('./db')
+const model = require('./Model')
 const app=express();
-// const Product=require('./data.json')
+const Product=require('./data.json')
 
-// app.get('/',async(req,res)=>{
-//     try {
-//         const {company,name,sort,select}=req.query;
-//         const obj={}
-//         if(company){
-//             obj.company=company;
-//         }
-//         if(name){
-//             obj.name=name;
-//         }
-//         apidata= model.find(obj)
-//         if(sort){
-//             let sortfix=sort.replace(","," ");
-//             apidata=apidata.sort(sortfix)
-//         }
-//         if(select){
-//             let selectfix=select.replace(","," ");
-//             apidata=apidata.select(selectfix)
-//         }
-//         console.log(obj)
-//         const data=await apidata
-//         res.send(data)
-//        } catch (error) {
-//         console.log(error)
-//        }
-// })
-app.get('/',(req,res)=>{
+app.get('/',async(req,res)=>{
+    try {
+        const {company,name,sort,select}=req.query;
+        const obj={}
+        if(company){
+            obj.company=company;
+        }
+        if(name){
+            obj.name=name;
+        }
+        apidata= model.find(obj)
+        if(sort){
+            let sortfix=sort.replace(","," ");
+            apidata=apidata.sort(sortfix)
+        }
+        if(select){
+            let selectfix=select.replace(","," ");
+            apidata=apidata.select(selectfix)
+        }
+        console.log(obj)
+        const data=await apidata
+        res.send(data)
+       } catch (error) {
+        console.log(error)
+       }
+})
+app.get('/about',(req,res)=>{
     res.send("Hello this is about page");
 })
-// const start=async()=>{
-//    try {
-//     await model.deleteMany()
-//     const data=await model.create(Product)
-//     console.log("Success")
-//    } catch (error) {
-//     console.log(error)
-//    }
-// }
+const start=async()=>{
+   try {
+    await model.deleteMany()
+    const data=await model.create(Product)
+    console.log("Success")
+   } catch (error) {
+    console.log(error)
+   }
+}
 
-// start()
-app.listen(3000,()=>{
+start()
+app.listen(3000||PORT,()=>{
     console.log("listening on 3000 port")
 })
